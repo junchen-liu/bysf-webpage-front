615bbe7c2a3babb21d60a6b18e292f408236614c
Junchen Liu <junchen_liu@126.com>
2022-01-18 18:38:00 -0500

    add homepage-related

--- src/components/Sidebars/DashboardSidebar.vue
+++ src/components/Sidebars/DashboardSidebar.vue
@@ -13,11 +13,28 @@
 		:class="['ant-layout-sider-' + sidebarColor, 'ant-layout-sider-' + sidebarTheme]"
 		theme="light"
 		:style="{ backgroundColor: 'transparent',}">
-			<div class="brand"><img src="images/logo-ct-black.png" alt=""> <span>Muse Dashboard</span></div>
+			<div class="brand">
+        <!--        company logo here-->
+        <span>祝梦教育</span>
+        <br>
+        <span style="color: rgb(126, 140, 141);" data-mce-style="color: #7e8c8d;">高校学子课堂小助手</span>
+      </div>
 			<hr>
 
 			<!-- Sidebar Navigation Menu -->
 			<a-menu theme="light" mode="inline">
+        <a-menu-item>
+          <router-link to="/homepage">
+						<span class="icon">
+							<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<path d="M3 4C3 3.44772 3.44772 3 4 3H16C16.5523 3 17 3.44772 17 4V6C17 6.55228 16.5523 7 16 7H4C3.44772 7 3 6.55228 3 6V4Z" fill="#111827"/>
+								<path d="M3 10C3 9.44771 3.44772 9 4 9H10C10.5523 9 11 9.44771 11 10V16C11 16.5523 10.5523 17 10 17H4C3.44772 17 3 16.5523 3 16V10Z" fill="#111827"/>
+								<path d="M14 9C13.4477 9 13 9.44771 13 10V16C13 16.5523 13.4477 17 14 17H16C16.5523 17 17 16.5523 17 16V10C17 9.44771 16.5523 9 16 9H14Z" fill="#111827"/>
+							</svg>
+						</span>
+            <span class="label">首页</span>
+          </router-link>
+        </a-menu-item>
 				<a-menu-item>
 					<router-link to="/course">
 						<span class="icon">
@@ -165,9 +182,16 @@
 		},
 		data() {
 			return {
-				// sidebarCollapsedModel: this.sidebarCollapsed,
+				sidebarCollapsedModel: this.sidebarCollapsed,
+        collapsed:false
 			}
 		},
+    methods: {
+      toggleCollapsed() {
+        this.collapsed = !this.collapsed;
+        console.log(this.collapsed)
+      },
+    },
 	})
 
 </script>
---
+++ src/views/page-components/home.vue
@@ -0,0 +1,16 @@
+<!--首页-->
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+}
+</script>
+
+<style scoped>
+
+</style>
