5554ed7d119978fa443584737a2c7b3d85a44717
Teresa Zhang <lingyiz4@illinois.edu>
2022-02-03 00:22:17 -0600

    update favorites

--- src/views/page-components/my/favorites.vue
+++ src/views/page-components/my/favorites.vue
@@ -1,13 +1,77 @@
 <template>
-  <div>
-    课表
-  </div>
+    <div>
+
+  <a-table :columns="columns" :data-source="data">
+    <span slot="action">
+      <a>Delete</a>
+      <a-divider type="vertical" />
+      <a class="ant-dropdown-link"> More actions <a-icon type="down" /> </a>
+    </span>
+  </a-table>
+
+
+    </div>
 </template>
 
+
 <script>
+
+const columns = [
+  {
+    dataIndex: 'name',
+    title: '标题',
+    key: 'name',
+    slots: { title: 'customTitle' },
+    scopedSlots: { customRender: 'title' },
+  },
+  {
+    title: '状态',
+    dataIndex: 'status',
+    key: 'status',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'date',
+    key: 'date',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    scopedSlots: { customRender: 'action' },
+  },
+];
+
+const data = [
+  {
+    key: '1',
+    name: '标题1',
+    status: '过期',
+    date: '2021-01-02',
+  },
+  {
+    key: '2',
+    name: '标题2',
+    status: '未知',
+    date: '2021-01-02',
+  },
+  {
+    key: '3',
+    name: '标题3',
+    status: '未过期',
+    date: '2021-01-02',
+  },
+];
+
 export default {
-  name: "favorites"
-}
+  name: "favorite",
+  data() {
+      return {
+        data,
+        columns,
+      };
+    },
+  };
+
 </script>
 
 <style scoped>
